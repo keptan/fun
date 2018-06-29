928b9d217fe0cc4db40980e48efcb8556791df0a
keptan <keptan@installgentoo.com>
2018-06-28 16:15:28 -1000

    some formatting

--- sort.h
+++ sort.h
@@ -36,14 +36,14 @@ bool sorted (const List<T> l, const F compare = ordOverload)
 	if(length(l) < 2)
 		return true; 
 
-	if(! compare( peek(pop(l)), peek(l)))
+	if(! compare( peek( pop(l)), peek(l)))
 		return false; 
 
 	return sorted(pop(l));
 }
 
 template <typename T> 
-List<List<T>> shuffleSplit (const List<T> l, const List<T> a= List<T>(), const List<T> b= List<T>())
+List<List<T>> shuffleSplit (const List<T> l, const List<T> a = List<T>(), const List<T> b = List<T>())
 {
 	const auto compare = [](const auto a, const auto b){ return length(a) > length(b);};
 
@@ -60,24 +60,6 @@ List<List<T>> shuffleSplit (const List<T> l, const List<T> a= List<T>(), const L
 	return shuffleSplit (pop(l), push(smaller, peek(l)), larger);
 }
 
-/*
-template <typename T, typename F = Ord<T>>
-List<T> mergeOrdered (const List<T> a, const List<T> b, const F compare = ordOverload, const List<T> out = List<T>())
-{
-	if(length(a) == 0 ||  length(b) == 0)
-		return push_back(out, push(a, b));
-
-	const auto c = [=](const auto first, const auto second){ return compare(peek_back(first), peek_back(second));};
-
-	const List<T> larger =  max(a,b,c);
-	const List<T> smaller = min(a,b,c);
-
-	mergeCounter++;
-
-	return mergeOrdered(pop_back(larger), smaller,  compare, push(out, peek_back(larger)));
-}
-*/
-
 template <typename T, typename F = Ord<T>>
 List<T> mergeOrdered (const List<T> a, const List<T> b, const F compare = ordOverload)
 {
@@ -89,7 +71,7 @@ List<T> mergeOrdered (const List<T> a, const List<T> b, const F compare = ordOve
 	const List<T> larger =  max(a,b,c);
 	const List<T> smaller = min(a,b,c);
 
-	return push(List<T>(peek(smaller)), mergeOrdered(pop(smaller), larger));
+	return push(List<T>( peek(smaller)), mergeOrdered( pop(smaller), larger));
 }
 
 template <typename T, typename F = Ord<T>>
@@ -99,8 +81,8 @@ List<T> mergeSort (const List<T> l, const F compare = ordOverload)
 		return l; 
 
 	const auto split = shuffleSplit(l); 
-	const auto first  = mergeSort(peek(split), compare); 
-	const auto second = mergeSort(peek_back(split), compare); 
+	const auto first  = mergeSort( peek(split), compare); 
+	const auto second = mergeSort( peek_back(split), compare); 
 
 	return mergeOrdered(first,second, compare);
 }
